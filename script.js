ee233086a887e16b8bd06defb0c71ea79bb1bdc1
Paul DelPadre <PDelPadre@rice.local>
2020-06-12 20:36:08 -0400

    Uploading docs

---
+++ script.js
@@ -0,0 +1,57 @@
+// Variables
+
+
+
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+
+
+
+
+// *********************************************************************
+// You should set your questions as an object like this example:
+// var questions = [
+//   {
+//     title: "Example Question 1:",
+//     choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+//     answer: "answer from choices"
+//   },
+//   {
+//     title: "Example Question 2:",
+//     choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+//     answer: "answer from choices"
+//   }
+// ];
+// ************************************************************************
+
+
+
+// ***************************************************************************
+// ✓ The first view of the application displays a button used to start the quiz.
+// ✓ Clicking the 'Start Quiz' button displays a series of questions.
+// ✓ Once the quiz begins, a countdown timer starts.
+// ✓ If a question is answered incorrectly, additional time is subtracted from the timer.
+// ✓ The timer ends when all questions have been answered or the timer reaches 0.
+// ✓ After the game ends, the user can save their initials and score to a highscores view using local storage.
+// ✓ Repository contains quality README with description, screenshot, link to deployed application.
+// ***************************************************************************
+
